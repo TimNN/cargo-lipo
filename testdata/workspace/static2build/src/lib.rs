24d82a8f8a4106587533f56bb6b5e071fe019118
Tim Neumann <mail@timnn.me>
2019-01-01 19:07:42 +0100

    add missing #[no_mangle] testdata

--- testdata/workspace/static2build/src/lib.rs
+++ testdata/workspace/static2build/src/lib.rs
@@ -1,3 +1,4 @@
+#[no_mangle]
 pub extern "C" fn hello_from_static2() {
     println!("Calling Build Util from Static 2!");
     buildutil::hello_from_build_util();
--- testdata/workspace/static3bin/src/lib.rs
+++ testdata/workspace/static3bin/src/lib.rs
@@ -2,6 +2,7 @@ pub fn internal_hello() {
     println!("Internal Hello from Static 3!");
 }
 
+#[no_mangle]
 pub extern "C" fn hello_from_static3() {
     println!("Calling Internal from Static 3 lib!");
     internal_hello();
