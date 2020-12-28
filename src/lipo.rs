c1121fad3fda4af4fac95f704fd46e9fe3f44ea6
patr0nus <dk4rest@gmail.com>
2020-12-29 01:51:59 +0800

    Check if the universal library is updated

--- src/lipo.rs
+++ src/lipo.rs
@@ -5,6 +5,18 @@ use failure::ResultExt;
 use log::info;
 use std::fs;
 use std::process::Command;
+use std::path::Path;
+
+fn is_output_updated(output: impl AsRef<Path>, inputs: impl IntoIterator<Item=impl AsRef<Path>>) -> std::io::Result<bool> {
+    let output_mtime = fs::metadata(output)?.modified()?;
+    for input in inputs {
+        let input_mtime = fs::metadata(input)?.modified()?;
+        if input_mtime > output_mtime {
+            return Ok(false)
+        }
+    }
+    Ok(true)
+}
 
 pub(crate) fn build(cargo: &Cargo, meta: &Meta, targets: &[impl AsRef<str>]) -> Result<()> {
     for package in meta.packages() {
@@ -37,13 +49,18 @@ pub(crate) fn build(cargo: &Cargo, meta: &Meta, targets: &[impl AsRef<str>]) -> 
 
         output.push(&lib_name);
 
-        let mut cmd = Command::new("lipo");
-        cmd.arg("-create").arg("-output").arg(output);
-        cmd.args(inputs);
+        if let Ok(true) = is_output_updated(&output, &inputs) {
+            info!("Universal library for {} is updated", package.name());
+        }
+        else {
+            let mut cmd = Command::new("lipo");
+            cmd.arg("-create").arg("-output").arg(output);
+            cmd.args(inputs);
 
-        info!("Creating universal library for {}", package.name());
+            info!("Creating universal library for {}", package.name());
 
-        crate::exec::run(cmd)?;
+            crate::exec::run(cmd)?;
+        }
     }
 
     Ok(())
