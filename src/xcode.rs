627eeaf0c7f4b4f09cd29492baad7219697acc9b
Daniel Brotsky <dev@brotsky.com>
2022-01-03 15:01:59 -0800

    Add awareness of the special simulator ABI.
    This fixes TimNN/cargo-lipo#52.

--- src/xcode.rs
+++ src/xcode.rs
@@ -24,15 +24,18 @@ pub(crate) fn integ(meta: &Meta, mut invocation: Invocation) -> Result<()> {
 
 fn targets_from_env() -> Result<Vec<String>> {
     let archs = env::var("ARCHS").with_context(|e| format!("Failed to read $ARCHS: {}", e))?;
-    let target_platform = match env::var("PLATFORM_NAME").as_ref().map(String::as_str) {
-        Ok("macosx") => "apple-darwin",
-        _ => "apple-ios",
+    let (platform, abi) = match env::var("PLATFORM_NAME").as_ref().map(String::as_str) {
+        Ok("macosx") => ("apple-darwin", None),
+        // There's a new "iphonesimulator" platform that requires a special abi suffix
+        // We just look for "sim" here as a minimal distinguishing check from "iphoneos".
+        Ok(s) if s.contains("sim") => ("apple-ios", Some("sim")),
+        _ => ("apple-ios", None)
     };
     Ok(archs
         .split(" ")
         .map(|a| a.trim())
         .filter(|a| !a.is_empty())
-        .map(|a| map_arch_to_target(a, target_platform))
+        .map(|a| map_arch_to_target(a, platform, abi))
         .collect::<Result<Vec<_>>>()
         .with_context(|e| format!("Failed to parse $ARCHS: {}", e))?)
 }
@@ -41,7 +44,7 @@ fn is_release_configuration() -> bool {
     env::var("CONFIGURATION").map(|v| v == "Release").unwrap_or(false)
 }
 
-fn map_arch_to_target(arch: &str, target_platform: &str) -> Result<String> {
+fn map_arch_to_target(arch: &str, platform: &str, abi: Option<&str>) -> Result<String> {
     let mapped_arch = match arch {
         "armv7" => "armv7",
         "arm64" => "aarch64",
@@ -49,7 +52,16 @@ fn map_arch_to_target(arch: &str, target_platform: &str) -> Result<String> {
         "x86_64" => "x86_64",
         _ => bail!("Unknown arch: {:?}", arch),
     };
-    Ok(format!("{}-{}", mapped_arch, target_platform))
+    match abi {
+        // the "sim" abi is only used with aarch64 builds
+        Some("sim") => if mapped_arch == "aarch64" {
+            Ok(format!("{}-{}-{}", mapped_arch, platform, "sim"))
+        } else {
+            Ok(format!("{}-{}", mapped_arch, platform))
+        },
+        Some(abi) => Ok(format!("{}-{}-{}", mapped_arch, platform, abi)),
+        None => Ok(format!("{}-{}", mapped_arch, platform)),
+    }
 }
 
 pub(crate) fn sanitize_env(cmd: &mut Command) {
